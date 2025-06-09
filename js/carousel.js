9398e944ae3cda709b0204774afa1c2cc0016ae3
sademarquez <151792037+sademarquez@users.noreply.github.com>
2025-06-08 19:08:26 -0500

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -2,7 +2,7 @@
 
 let currentSlide = 0;
 let autoSlideInterval;
-const slideDuration = 1400; // Duración de cada slide en milisegundos
+const slideDuration = 2300; // Duración de cada slide en milisegundos
 
 let carouselTrack;
 let carouselDotsContainer;
