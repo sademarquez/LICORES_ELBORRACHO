4a45de28416637d3cae6a111098c2fd2983b2ae5
sademarquez <sademarquez@gmail.com>
2025-07-18 14:53:05 -0500

    fix(carousel): Añade dependencia de autoplay y despliega versión final

--- el-borracho-pwa/src/components/HeroCarousel.jsx
+++ el-borracho-pwa/src/components/HeroCarousel.jsx
@@ -1,68 +1,23 @@
-
-import React, { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import React from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay';
 import './HeroCarousel.css';
 
-const variants = {
-  enter: (direction) => ({
-    x: direction > 0 ? 1000 : -1000,
-    opacity: 0
-  }),
-  center: {
-    zIndex: 1,
-    x: 0,
-    opacity: 1
-  },
-  exit: (direction) => ({
-    zIndex: 0,
-    x: direction < 0 ? 1000 : -1000,
-    opacity: 0
-  })
-};
-
 export const HeroCarousel = ({ banners }) => {
-  const [[page, direction], setPage] = useState([0, 0]);
-
-  const paginate = (newDirection) => {
-    setPage([page + newDirection, newDirection]);
-  };
-
-  const imageIndex = ((page % banners.length) + banners.length) % banners.length;
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      paginate(1);
-    }, 5000); // Change slide every 5 seconds
-    return () => clearInterval(timer);
-  }, [page]);
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()]);
 
   return (
-    <div className="hero-carousel-container">
-      <AnimatePresence initial={false} custom={direction}>
-        <motion.img
-          key={page}
-          src={banners[imageIndex].imageUrl}
-          custom={direction}
-          variants={variants}
-          initial="enter"
-          animate="center"
-          exit="exit"
-          transition={{
-            x: { type: "spring", stiffness: 300, damping: 30 },
-            opacity: { duration: 0.2 }
-          }}
-          className="hero-image"
-        />
-      </AnimatePresence>
-      <div className="hero-content">
-        <h2>{banners[imageIndex].title}</h2>
-        <p>{banners[imageIndex].subtitle}</p>
-      </div>
-      <div className="next" onClick={() => paginate(1)}>
-        {'‣'}
-      </div>
-      <div className="prev" onClick={() => paginate(-1)}>
-        {'‣'}
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        {banners.map((banner, index) => (
+          <div className="embla__slide" key={index}>
+            <img className="embla__slide__img" src={banner.imageUrl} alt={banner.title} />
+            <div className="embla__slide__content">
+              <h2>{banner.title}</h2>
+              <p>{banner.subtitle}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
